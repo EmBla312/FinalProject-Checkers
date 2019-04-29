f2db76a89175386f1d89c3629c4753f927436b10
CFettig <27834526+CFettig@users.noreply.github.com>
2019-04-29 15:29:02 -0500

    Added JavaDoc comments to GameData class.

--- FinalProject-Checkers/src/game/GameData.java
+++ FinalProject-Checkers/src/game/GameData.java
@@ -3,21 +3,43 @@ import java.util.LinkedList;
 import java.util.Scanner;
 
 public class GameData {
-	
+	/**
+	 * The int value of an empty tile.
+	 */
 	private static final int EMPTY = 0;
+	/**
+	 * The int value of a tile containing a red pawn.
+	 */
 	public static final int R_PAWN = 1;
+	/**
+	 * The int value of a tile containing a white pawn.
+	 */
 	public static final int W_PAWN = 3;
+	/**
+	 * The int value of a tile containing a red king.
+	 */
 	private static final int R_KING = 2;
+	/**
+	 * The int value of a tile containing a white king.
+	 */
 	private static final int W_KING = 4;
-
+	/**
+	 * An 8 by 8 array to represent the checker board.
+	 */
 	private int[][] board; 
 
+	/**
+	 * Creates the board array and calls setUpBoard to place piece values.
+	 */
 	public GameData() {
 
 		board = new int[8][8];
 		setUpBoard();
 	}
 	
+	/**
+	 * Places pieces in the appropriate tiles for the beginning of the game.
+	 */
 	public void setUpBoard() {
 
 		for (int row = 0; row < 8; row++) {
@@ -37,27 +59,62 @@ public class GameData {
 		}
 	}//end setUpBoard()
 	
+	/**
+	 * Determines whether the game is over.
+	 * @param team The player whose legal moves need to be evaluated.
+	 * @return true if the given player can make no legal moves.
+	 */
 	public boolean isGameOver(int team) {
 		PieceMove[] legalMoves = getLegalMoves(team);
 		return legalMoves == null;
 	}
-	public int pieceAt(int row, int col) {
-		
-		return board[row][col];		
-	}
+//	/**
+//	 * 
+//	 * @param row
+//	 * @param col
+//	 * @return
+//	 */
+//	public int pieceAt(int row, int col) {
+//		
+//		return board[row][col];		
+//	}
+	/**
+	 * Sets the element at the coordinates to the piece variable.
+	 * @param row This is the row index of the array space.
+	 * @param col This is the column index of the array space.
+	 * @param piece This is the value to set as the element at the given indices.
+	 */
 	public void setPieceAt(int row, int col, int piece) {
 		
 		board[row][col] = piece;
 	}
 	
+	/**
+	 * Determines whether an element is a white king. 
+	 * @param fromRow This is the row index of the tile to be checked.
+	 * @param fromCol This is the col index of the tile to be checked.
+	 * @return True if the given indices contain a white king.
+	 */
 	public boolean isKing(int fromRow, int fromCol) {
 		return board[fromRow][fromCol] == W_KING;
 	}
+	
+	/**
+	 * Gets indices from a PieceMove object and passes them to the private makeMove method.
+	 * @param move This is the PieceMove object that contains the coordinates.
+	 */
 	public void makeMove(PieceMove move) {
 		
 		makeMove(move.getFromRow(), move.getFromCol(), move.getToRow(), move.getToCol());
 	}
-
+	
+	/**
+	 * Checks whether move is jump or regular move. Moves the piece int from the current tile to the new tile and sets the current tile to 0 for empty.
+	 * @param fromRow This is the row index of the tile the piece starts at.
+	 * @param fromCol This is the col index of the tile the piece starts at.
+	 * @param toRow This is the row index of the tile the piece will be moved to.
+	 * @param toCol This is the col index of the tile the piece will be moved to.
+	 */
 	private void makeMove(int fromRow, int fromCol, int toRow, int toCol) {
 		
 		board[toRow][toCol] = board[fromRow][fromCol];
@@ -75,6 +132,11 @@ public class GameData {
 			board[toRow][toCol] = R_KING;
 	}
 										//team represented by the corresponding Pawn IE R_PAWN = 1
+	/**
+	 * Gets a list of all possible moves from the player's current position.
+	 * @param team Represents the player whose moves are being calculated.
+	 * @return an array of length 0 if linked list of moves is empty; else returns an array of all possible PieceMove objects.
+	 */
 	public PieceMove[] getLegalMoves(int team) {
 	 //the moves linkedlist can then be passed into a stack entry by entry.
 	
@@ -143,8 +205,13 @@ public class GameData {
 		       return moveArray;
 		}
 	} // end getLegalMoves()
-	
-	
+	/**
+	 * Gets all legal jumps for the player's piece at the given indices. Used for double-jumping.
+	 * @param team Represents the player whose jumps are being calculated.
+	 * @param row This is the Row index of the tile that contains the piece.
+	 * @param col This is the column index of the tile that contains the piece.
+	 * @return an array of all possible jumps for that single piece.
+	 */
 	public PieceMove[] getLegalJumpsFrom(int team, int row, int col) {
 		
 		
@@ -178,7 +245,17 @@ public class GameData {
 	    }
 	}	//end legalJumps
 	
-	
+	/**
+	 * Tests whether a piece can jump to the given tile.
+	 * @param team Represents the player whose piece is jumping.
+	 * @param fromRow This is the row index of the tile the piece starts at.
+	 * @param fromCol This is the column index of the tile the piece starts at.
+	 * @param middleRow This is the row index of the tile that should hold an opponent's piece to jump.
+	 * @param middleCol This is the column index of the tile that should hold an opponent's piece to jump.
+	 * @param toRow This is the row index of th tile immediately after the middle tile.
+	 * @param toCol This is the col index of th tile immediately after the middle tile.
+	 * @return true if (middleRow, middleCol) contains opponent's piece and (toRow, toCol) is empty.
+	 */
 	private boolean canJump(int team, int fromRow, int fromCol, int middleRow, int middleCol, int toRow, int toCol) {
 	
 
@@ -205,7 +282,16 @@ public class GameData {
 
 	}  // end canJump()
 	
-
+	/**
+	 * Called by the getLegalMoves method to test whether the player can legally move from (fromRow,fromCol) to (toRow,toCol).
+	 * It is assumed that (fromRow,toRow) contains one of the team's pieces and that (toRow,toCol) is a neighboring tile.
+	 * @param team Represents the player whose piece is moving.
+	 * @param fromRow This is the row index of the tile the piece starts at.
+	 * @param fromCol This is the column index of the tile the piece starts at.
+	 * @param toRow This is the row index of the tile to check for validity.
+	 * @param toCol This is the column index of the tile to check for validity.
+	 * @return true if (toRow, toCol) is within array bounds, empty, and in the correct direction for the given team.
+	 */
 	private boolean canMove(int team, int fromRow, int fromCol, int toRow, int toCol) {
 	       // This is called by the getLegalMoves() method to determine whether
 	       // the team can legally move from (fromRow,fromCol) to (toRow,toCol).  It is
@@ -232,6 +318,12 @@ public class GameData {
 	    
 	 }  // end canMove()
 	
+	/**
+	 * Evaluates each PieceMove in an array of PieceMoves and gives it an integer weight.
+	 * @param legalMoves This is the array of PieceMoves to be evaluated.
+	 * @param position This is the index of the PieceMove object to be evaluated.
+	 * @return the weight of of the PieceMove based on whether it's a jump or a regular move, and whether the piece is a king.
+	 */
 	public int evaluateMove(PieceMove[] legalMoves, int position) {
 		int weight = 0;
 		
@@ -276,7 +368,9 @@ public class GameData {
 		}
 		return board_weight;
 	}
-	
+	/**
+	 * Prints the board for displaying.
+	 */
 	public void printBoard() {
 		
 		System.out.println("   0 1 2 3 4 5 6 7");		
@@ -291,7 +385,11 @@ public class GameData {
 		}
 		
 	}
-	
+	/**
+	 * Copies a GameData object.
+	 * @param original This is the GameData object to be copied.
+	 * @return an identical GameData object.
+	 */
 	public GameData copy(GameData original) {
 		GameData copy = new GameData();
 		for (int row = 0; row < original.board.length; row++) {
@@ -302,6 +400,12 @@ public class GameData {
 		return copy;
 	}
 	
+	/**
+	 * Gets a list of all the boards that are possible 1 move into the future from a current board.
+	 * @param board This is the initial state of the board.
+	 * @param team This is the player whose pieces are moving.
+	 * @return a linked list of boards.
+	 */
 	public LinkedList<GameData> getFutureBoards(GameData board, int team) {
 		PieceMove[] depth1array = board.getLegalMoves(team);
 		
